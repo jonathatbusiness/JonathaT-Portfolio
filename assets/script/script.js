5069c9fc6f81482f6e8456d9506fb89def757567
Jonatha L Teixeira <106690382+jonathatbusiness@users.noreply.github.com>
2022-09-12 09:13:23 -0300

    Added some codes to Home

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,22 @@
+import Sessions from "./Sessions.js";
+
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 
 btn.onclick = function () {
   sidebar.classList.toggle("active");
 };
+
+//Sessions
+const homeContent = new Sessions("Hi, I'm Jonatha.", "Contact Me");
+
+const homeSession = `
+<header><span class="titleHome">${homeContent.sessionTitleHome}</span></header>
+
+            <a class="btnContactme">
+                <div class="btnContainer"><span class="btnText">${homeContent.sessionButton}</span></div>
+`;
+
+const main = document.querySelector(".maincontent");
+
+main.innerHTML = homeSession;
